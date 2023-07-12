0af1c8750d0cba575e87de6ea304ddbe829f7356
Aditya <pundaditya62@gmail.com>
2023-07-13 02:11:48 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer(){
+
+
+    return(
+        <footer>
+            <p>Copyright |  Aditya Pund </p>
+        </footer>
+    )
+}
+
+export default Footer;
